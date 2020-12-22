e0c066a9760878bc9185fcb7004ae1a968d59787
Brian Young <Brian.Young@sacw.com>
2020-12-22 11:48:18 -0600

    progress rest added tr elements starting on button click

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+let tbody = d3.select("tbody");
+
+tableData.forEach(function(sighting) {
+  let row = tbody.append("tr");
+  Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+  });
+});
+
+let button = d3.select("#filter-btn");
+let inputField = d3.select("#datetime").value;
+
+button.on("click", function() {
+    console.log(inputField);
+});
